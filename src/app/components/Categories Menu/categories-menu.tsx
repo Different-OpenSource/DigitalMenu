10f71d17bd4eea0bef5ca6a6b7ee2010f9f494d9
Luiz Mello <luiz.mello@poatek.com>
2023-05-14 21:36:41 -0300

    Created new Categories menu component

---
+++ src/app/components/Categories Menu/categories-menu.tsx
@@ -0,0 +1,34 @@
+import React, { useState, FC } from 'react';
+import './categories-menu.css';
+interface Props {
+  categories: string[];
+  filterItems: (category: string) => void;
+}
+
+const Categories: FC<Props> = ({ categories, filterItems }) => {
+  let [currentCategory, setCurrentCategory] = useState<number>(0);
+
+  const handleCategoryClick = (categoryIndex: number, category: string) => {
+    filterItems(category);
+    setCurrentCategory(categoryIndex);
+  };
+
+  return (
+    <div className="btn-container">
+      {categories.map((category: string, index: number) => {
+        return (
+          <button
+            type="button"
+            className={`filter-btn ${index === currentCategory ? 'active' : ''}`}
+            key={index}
+            onClick={() => handleCategoryClick(index, category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
